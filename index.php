44d4a9995854b2be30951de9021fc38d7206a7d3
Pankaj Kumar <33214327+pkburkoo@users.noreply.github.com>
2017-12-07 12:21:51 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,213 @@
+<html>
+<body>
+.section_ryk_4_book_curs {
+    background: url(http://behavior-dev-01.com/wp-content/uploads/2017/11/bg-5.jpg);
+    background-repeat: no-repeat;
+    margin-top: -120px;
+    margin-bottom: -50px!important;
+}
+
+==
+
+<h3 class="lft_hding lft_hding1 font_norml">Få et uforpligtende<br />
+tilbud på din<br />
+entreprise</h3>
+<div class="green_dot_line fllk"></div>
+<h4 class="cont_form_top_headeing">Udfyld formularen, og få et<br />
+uforpligtende tilbud på din opgave.<br />
+Vi vender tilbage hurtigst muligt.</h4>
+
+
+====
+
+on the front page in the contact form the top text should be changed to 
+
+Få et uforpligtende tilbud på din entreprise
+
+===
+
+ALLE SENDER DERES GAMECHANGERS TIL OS
+
+Og der er ingen begrænsning ift. personer, lande eller perspektiver vi kan inddrage
+
+VI LAVER JERES HELT EGET SÆT TRENDKORT
+
+Vi kigger både på de trends I spotter og de trends I ikke spotter, så vi får blinde vinkler, tabuer og udefra-ind perspektivet med
+
+<div class="intr_pg_evnt">
+<h1>#1</h1>
+</div>
+<div class="h3_intr_pag_vevnt">
+<h3 style="font-weight:normal"; class="uppercase">FØRST FÅR I EN INVITATION TIL AT SPOTTE EN GAMECHANGER</h3>
+</div>
+<p>En gamechanger er den trend, der får fremtiden til at rykke rigtig tæt på</p>
+
+
+
+http://www.jimhumbleaudios.com
+
+<div class="info_box_dve">
+<div class="intr_pg_evnt">
+<h1>#1</h1>
+</div>
+<div class="h3_intr_pag_vevnt">
+<h3 style="font-weight:normal"; class="uppercase">FØRST FÅR I EN INVITATION TIL AT SPOTTE EN GAMECHANGER</h3>
+</div>
+<p>En gamechanger er den trend, der får fremtiden til at rykke rigtig tæt på</p>
+</div>
+
+==
+
+
+
+<div class="info_box_dve">
+<div class="intr_pg_evnt">
+<h1>#10</h1>
+</div>
+<div class="h3_intr_pag_vevnt">
+<h3 style="font-weight:normal"; class="uppercase">TIL SIDST FÅR I TRENDKORT OG RAPPORT</h3>
+</div>
+<p>Vi samler alt materiale incl. en trendmapping rapport på jeres kundeside hos os</p>
+</div>
+
+====
+
+<div class="map_area_header">
+<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2254.2669938185454!2d12.585160415740692!3d55.59736681143699!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4653aad444ac7899%3A0x4c9785eba923a5e4!2sBielefeldt+ApS!5e0!3m2!1sen!2sin!4v1511010946527" width="700" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+</div>
+
+===
+
+
+<div class="map_area_header">
+<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2254.2669938185454!2d12.585160415740692!3d55.59736681143699!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4653aad444ac7899%3A0x4c9785eba923a5e4!2sBielefeldt+ApS!5e0!3m2!1sen!2sin!4v1511010946527" width="700" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+</div>
+
+
+++++++++++++++++++++++++
+
+can you set up tracking for the contact form?
+When you filled out the contact form this should be the text: Tak for din henvendelse. Vi kontakter dig indenfor 24 timer eller næstkommende hverdag. Rigtig god dag.
++++++++++++++++
+
+
+<i class="icon-phone phn_imgbig" aria-hidden="true"></i>
+
+Prøv vores interaktive foredrag og workshops
+
+Workshops
+===========
+
+
+<div class="xztitle_mid_dash title_mid_dash_acdmy" style=""><strong class="acedmy_strong_ttl">MED FOKUS PÅ FREMTIDEN</strong></div>
+<div class="dc_dotall dc_dotall_acadmy" style="font-size:12px;">
+<p class="dc_dotall_p_acadmy3">Når et foredrag er for lidt og et helt projekt for meget, så er workshops bare sagen!</p>
+<p class="dc_dotall_p_acadmy3">På en typisk workshop vil vi arbejde med jeres vigtigste trends via et sæt trendkort, oplæg om jeres fremtid, dialog og provokation, samt fremtidsforsknings-metoder og overblik. Hos os er kunderne helt unikke med deres egne originale tanker og perspektiver. Der vil altid være velafprøvede metoder, vi kan bruge, men vi får det største kick ud af at sætte en workshop sammen til lige præcist jer!</p>
+</div>
+
+Når et foredrag er for lidt og et helt projekt for meget, så er workshops bare sagen!
+
+På en typisk workshop vil vi arbejde med jeres vigtigste trends via et sæt trendkort, oplæg om jeres fremtid, dialog og provokation, samt fremtidsforsknings-metoder og overblik. Hos os er kunderne helt unikke med deres egne originale tanker og perspektiver. Der vil altid være velafprøvede metoder, vi kan bruge, men vi får det største kick ud af at sætte en workshop sammen til lige præcist jer!
+=========
+
+- Når workshoppen er slut og du er rigtig, rigtig glad, så er det fordi at …
+
+- Hvor meget vil I gerne selv involveres i planlægningen? Skal vi tage hele styringen og designe det hele for jer, eller har I allerede en masse idéer, som vi skal fylde indhold i?
+
+- Hvad er det allervigtigste spørgsmål eller problem, vi skal arbejde med?
+
+
+<span class="arrow_bottom_workshop_left">
+<img src="http://behavior-dev-01.com/wp-content/uploads/2017/12/arrow-5.png">
+</span>
+<span class="arrow_bottom_workshop_right">
+<img src="http://behavior-dev-01.com/wp-content/uploads/2017/12/arrow-6.png">
+</span>
+.arrow_workshop_1, .arrow_workshop_2, .arrow_workshop_3, .arrow_workshop_4{
+	position:absolute;
+}
+
+.workshop_first_section .bg, .workshop_second_section .bg, .workshop_third_section .bg, .hardu_bru_workshop_section .bg, .last_section_workshoppg .bg {
+    background-repeat: no-repeat;
+    background-size: 100% 100%!important;
+}
+
+
+.arrow_bottom_workshop_left {
+ left: 20px;
+}
+.arrow_bottom_workshop_right {
+right: 50px;
+}
+.arrow_workshop_left {
+    left: -10px;
+}
+.arrow_workshop_right {
+    right: -10px;
+}
+span.arrow_workshop_2 {
+    right: 10px;
+    top: -25px;
+    z-index: 9;
+}
+span.arrow_workshop_1 {
+    right: 90px;
+}
+
+
+
+https://www.google.co.in/search?dcr=0&ei=aRghWrvaBMbQvgSiiJLgAQ&q=show+address+in+google+maps+php&oq=show+address+in+google+maps+php&gs_l=psy-ab.3.0.0i22i30k1.1738.2183.0.4078.4.4.0.0.0.0.166.166.0j1.1.0....0...1c.1.64.psy-ab..3.1.165....0.uEzsuG1q1RM
+
+https://network.convergenceservices.in/forum/19-php/4163-show-google-maps-using-address-dynamically-in-php.html
+
+http://webexplorar.com/display-each-users-location-in-google-map-using-address-in-php-and-java-script/
+
+https://stackoverflow.com/questions/12123164/address-in-php-variable-google-maps-embedding-without-javascript
+
+https://www.google.co.in/search?dcr=0&ei=ZhshWrPmAorevAS3r6TgAQ&q=insert+query+in+php&oq=insert+&gs_l=psy-ab.3.0.0i67k1l2j0l8.748998.749866.0.751411.7.7.0.0.0.0.351.1004.3-3.3.0....0...1c.1.64.psy-ab..4.3.1002....0.Oqwe4JwcTTk
+
+https://stackoverflow.com/questions/21320524/logout-php-script
+
+https://stackoverflow.com/questions/17138561/insert-into-mysql-table-php
+====
+
+.workshop_first_row_gola .text-box-circle {
+    width: 90%!important;
+    margin: 0px 0px 0px 0px!important;
+}
+.workshop_first_row_gola .text-box-square .text-inner, .text-box-circle .text-inner {
+top: 27%;
+}
+-----
+
+can you change the background on this webpage https://murerfirmaetbielefeldt.dk
+to white
+can I change a phone number on this page?
+
+
+60140204
+
+
+<div class="secnd_title_bot">
+<h3 class="section-title section-title-center"><b></b><span class="section-title-main">FÅ ET UFORPLIGTENDE TILBUD - RING <i class="icon-phone phn_imgbig phn_imgbigomos" aria-hidden="true"></i> 60 14 02 04</span><b></b></h3>
+</div>
+
+roboto
+
+yes - will you change the font to roboto
+?
+and also make the headlines not italic?
+
+===
+
+
+
+
+.front_page_rowfrem2 {
+    max-width: 100%!important;
+}
+p.front_page_fremp {
+    padding: 0 30px;
+}
+</body>
+</html>
